fce8f75f1724b5e832dcd922b7519dda2ecce83d
DoctorRiceYouSee <jerico.horca@gmail.com>
2019-11-05 01:06:30 -0800

    Added skeleton code

---
+++ libuthread/sem.c
@@ -0,0 +1,35 @@
+#include <stddef.h>
+#include <stdlib.h>
+
+#include "queue.h"
+#include "sem.h"
+#include "thread.h"
+
+struct semaphore {
+
+};
+
+sem_t sem_create(size_t count)
+{
+
+}
+
+int sem_destroy(sem_t sem)
+{
+
+}
+
+int sem_down(sem_t sem)
+{
+
+}
+
+int sem_up(sem_t sem)
+{
+
+}
+
+int sem_getvalue(sem_t sem, int *sval)
+{
+
+}
---
+++ libuthread/tps.c
@@ -0,0 +1,44 @@
+#include <assert.h>
+#include <pthread.h>
+#include <signal.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/mman.h>
+#include <unistd.h>
+
+#include "queue.h"
+#include "thread.h"
+#include "tps.h"
+
+
+int tps_init(int segv)
+{
+
+}
+
+int tps_create(void)
+{
+
+}
+
+int tps_destroy(void)
+{
+
+}
+
+int tps_read(size_t offset, size_t length, char *buffer)
+{
+
+}
+
+int tps_write(size_t offset, size_t length, char *buffer)
+{
+
+}
+
+int tps_clone(pthread_t tid)
+{
+
+}
