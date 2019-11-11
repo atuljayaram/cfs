0fe55d762d3e629c35280f83247815f556a57d99
DoctorRiceYouSee <jerico.horca@gmail.com>
2019-11-11 15:28:43 -0800

    This is the testing script that the professor provides us

--- test/tps.c
+++ test/tps.c
@@ -11,168 +11,101 @@
 static char msg1[TPS_SIZE] = "Hello world!\n";
 static char msg2[TPS_SIZE] = "hello world!\n";
 
-static sem_t sem1, sem2, sem3, sem4;
+static sem_t sem1, sem2;
 
-void *thread2(void* arg)
+void thread2(void arg)
 {
-	char *buffer = malloc(TPS_SIZE);
+        char *buffer = malloc(TPS_SIZE);
 
-	/* Create TPS and initialize with *msg1 */
-	assert(tps_create() == 0);
-	assert(tps_write(0, TPS_SIZE, msg1) == 0);
+        /* Create TPS and initialize with *msg1 */
+        tps_create();
+        tps_write(0, TPS_SIZE, msg1);
 
-	/* Read from TPS and make sure it contains the message */
-	memset(buffer, 0, TPS_SIZE);
-	tps_read(0, TPS_SIZE, buffer);
-	assert(!memcmp(msg1, buffer, TPS_SIZE));
-	printf("thread2: read OK!\n");
+        /* Read from TPS and make sure it contains the message */
+        memset(buffer, 0, TPS_SIZE);
+        tps_read(0, TPS_SIZE, buffer);
+        assert(!memcmp(msg1, buffer, TPS_SIZE));
+        printf("thread2: read OK!\n");
 
-	/* Transfer CPU to thread 1 and get blocked */
-	sem_up(sem1);
-	sem_down(sem2);
+        /* Transfer CPU to thread 1 and get blocked */
+        sem_up(sem1);
+        sem_down(sem2);
 
-	/* When we're back, read TPS and make sure it sill contains the original */
-	memset(buffer, 0, TPS_SIZE);
-	tps_read(0, TPS_SIZE, buffer);
-	assert(!memcmp(msg1, buffer, TPS_SIZE));
-	printf("thread2: read OK!\n");
+        /* When we're back, read TPS and make sure it sill contains the original */
+        memset(buffer, 0, TPS_SIZE);
+        tps_read(0, TPS_SIZE, buffer);
+        assert(!memcmp(msg1, buffer, TPS_SIZE));
+        printf("thread2: read OK!\n");
 
-	/* Transfer CPU to thread 1 and get blocked */
-	sem_up(sem1);
-	sem_down(sem2);
+        /* Transfer CPU to thread 1 and get blocked */
+        sem_up(sem1);
+        sem_down(sem2);
 
-	/* Destroy TPS and quit */
-	assert(tps_destroy() == 0);
-    free(buffer);
-	return NULL;
+        /* Destroy TPS and quit */
+        tps_destroy();
+        free(buffer);
+        return NULL;
 }
 
-void *thread1(void* arg)
+void thread1(void arg)
 {
-	pthread_t tid;
-	char *buffer = malloc(TPS_SIZE);
+        pthread_t tid;
+        char *buffer = malloc(TPS_SIZE);
 
-	/* Create thread 2 and get blocked */
-	pthread_create(&tid, NULL, thread2, NULL);
-	sem_down(sem1);
+        /* Create thread 2 and get blocked */
+        pthread_create(&tid, NULL, thread2, NULL);
+        sem_down(sem1);
 
-	/* When we're back, clone thread 2's TPS */
-	assert(tps_clone(tid) == 0);
+        /* When we're back, clone thread 2's TPS */
+        tps_clone(tid);
 
-	/* Read the TPS and make sure it contains the original */
-	memset(buffer, 0, TPS_SIZE);
-	assert(tps_read(0, TPS_SIZE, buffer) == 0);
-	assert(!memcmp(msg1, buffer, TPS_SIZE));
-	printf("thread1: read OK!\n");
+        /* Read the TPS and make sure it contains the original */
+        memset(buffer, 0, TPS_SIZE);
+        tps_read(0, TPS_SIZE, buffer);
+        assert(!memcmp(msg1, buffer, TPS_SIZE));
+        printf("thread1: read OK!\n");
 
-	/* Modify TPS to cause a copy on write */
-	buffer[0] = 'h';
-	tps_write(0, 1, buffer);
+        /* Modify TPS to cause a copy on write */
+        buffer[0] = 'h';
+        tps_write(0, 1, buffer);
 
-	/* Transfer CPU to thread 2 and get blocked */
-	sem_up(sem2);
-	sem_down(sem1);
+        /* Transfer CPU to thread 2 and get blocked */
+        sem_up(sem2);
+        sem_down(sem1);
 
-	/* When we're back, make sure our modification is still there */
-	memset(buffer, 0, TPS_SIZE);
-	tps_read(0, TPS_SIZE, buffer);
-	assert(!strcmp(msg2, buffer));
-	printf("thread1: read OK!\n");
+        /* When we're back, make sure our modification is still there */
+        memset(buffer, 0, TPS_SIZE);
+        tps_read(0, TPS_SIZE, buffer);
+        assert(!strcmp(msg2, buffer));
+        printf("thread1: read OK!\n");
 
-	/* Transfer CPU to thread 2 */
-	sem_up(sem2);
+        /* Transfer CPU to thread 2 */
+        sem_up(sem2);
 
-	/* Wait for thread2 to die, and quit */
-	pthread_join(tid, NULL);
-	tps_destroy();
-    
-    free(buffer);
-	return NULL;
-}
-
-void *thread4(void* arg)
-{
-  char *buffer = malloc(TPS_SIZE);
-
-
-  tps_create();
-
-	//write to tps
-  assert(tps_write(0, TPS_SIZE, msg1) == 0);
-	//switch to thread 3
-  sem_up(sem3);
-  sem_down(sem4);
-
-	//read from tps check message is still there
-  memset(buffer, 0, TPS_SIZE);
-  assert(tps_read(0, TPS_SIZE, buffer) == 0);
-  assert(!memcmp(msg1, buffer, TPS_SIZE));
-  sem_up(sem3);
-  sem_down(sem4);
-
-  free(buffer);
-  return NULL;
-
-}
-
-void *thread3(void* arg){
-
-    pthread_t tid;
-    pthread_create(&tid, NULL, thread4, NULL);
-    sem_down(sem3);
-		//clone thread4's tps
-    tps_clone(tid);
-		//destroy our reference
-    tps_destroy();
-
-		//switch to thead4
-    sem_up(sem4);
-    sem_down(sem3);
-
-    sem_up(sem4);
-
-    return NULL;
-}
-
-void test_clone_write()
-{
-  pthread_t tid;
-
-  /* Create two semaphores for thread synchro */
-  sem1 = sem_create(0);
-  sem2 = sem_create(0);
-
-  /* Create thread 1 and wait */
-  pthread_create(&tid, NULL, thread1, NULL);
-  pthread_join(tid, NULL);
-
-  /* Destroy resources and quit */
-  sem_destroy(sem1);
-  sem_destroy(sem2);
-}
-
-void test_destroy_clone(){
-    pthread_t tid;
-
-    /* Create two semaphores for thread synchro */
-    sem3 = sem_create(0);
-    sem4 = sem_create(0);
-
-    /* Create thread 1 and wait */
-    pthread_create(&tid, NULL, thread3, NULL);
-    pthread_join(tid, NULL);
-
-    /* Destroy resources and quit */
-    sem_destroy(sem3);
-    sem_destroy(sem4);
+        /* Wait for thread2 to die, and quit */
+        pthread_join(tid, NULL);
+        tps_destroy();
+        free(buffer);
+        return NULL;
 }
 
 int main(int argc, char **argv)
 {
-	assert(tps_init(1) == 0);
+        pthread_t tid;
 
-    test_clone_write();
-    test_destroy_clone();
-    
-	return 0;
+        /* Create two semaphores for thread synchro */
+        sem1 = sem_create(0);
+        sem2 = sem_create(0);
+
+        /* Init TPS API */
+        tps_init(1);
+
+        /* Create thread 1 and wait */
+        pthread_create(&tid, NULL, thread1, NULL);
+        pthread_join(tid, NULL);
+
+        /* Destroy resources and quit */
+        sem_destroy(sem1);
+        sem_destroy(sem2);
+        return 0;
 }
