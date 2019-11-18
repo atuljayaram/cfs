fd070a93be479780d37968d31a927f78334390e7
Atul Jayaram <atuljayaram@users.noreply.github.com>
2019-11-17 19:15:46 -0800

    Added Data Structures

--- libfs/fs.c
+++ libfs/fs.c
@@ -7,7 +7,54 @@
 #include "disk.h"
 #include "fs.h"
 
-/* TODO: Phase 1 */
+typedef unsigned __int128 uint128_t;
+
+struct superblock{
+	uint64_t signature;
+	uint16_t total_amount;
+	uint16_t root_index;
+	uint16_t data_index;
+	uint16_t num_blocks;
+	uint8_t num_FAT;
+	uint8_t padding[4079];
+}__attribute__((packed));
+
+struct superblock super;
+
+struct FAT{
+	uint16_t* arr;
+}__attribute__((packed));
+
+struct FAT our_fat;
+
+struct entries{
+	char filename[16];
+	uint32_t file_size;
+	uint16_t first_index;
+	uint8_t padding[10];
+}__attribute__((packed));
+
+struct rootdirectory{
+	 struct entries root[128];
+}__attribute__((packed));
+
+struct rootdirectory * our_root;
+
+struct fd_node{
+	int fd;
+	size_t offset;
+	const char* filename;
+};
+
+struct fd_table{
+	int fd_count;
+	struct fd_node descriptors[32];
+};
+
+struct fd* our_fd_table;
+
+//struct fd_node* return_fd(int fd); FOR DEBUGGING, MAYBE?
+//struct entries* return_entry(const char* filename); FOR DEBUGGING, MAYBE?
 
 int fs_mount(const char *diskname)
 {
