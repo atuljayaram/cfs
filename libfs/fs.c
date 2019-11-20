f4c844da0b6abe9c292a0a896f02be8d797e5ae4
Atul Jayaram <atuljayaram@users.noreply.github.com>
2019-11-19 17:20:10 -0800

    Fully implemented Phase 1

--- libfs/fs.c
+++ libfs/fs.c
@@ -52,32 +52,109 @@ struct fd_table{
 };
 
 struct fd* our_fd_table;
-fd_t currently_open[FS_OPEN_MAX_COUNT];
-
-/* TODO: Phase 1 */
+fd_t currently_open(FS_OPEN_MAX_COUNT);
 
 int fs_mount(const char *diskname)
 {
-	/* TODO: Phase 1 */
+	int i;
+  void * mount_buffer=NULL;
+  if(block_disk_open(diskname)==-1)
+    return -1;
+
+  
+  block_read(0,&super);
+
+  if(super.total_amount!=block_disk_count())
+    return -1;
+  if(super.num_blocks!=super.total_amount -2-super.num_FAT)
+    return -1;
+  if(super.root_index!=super.num_FAT+1)
+    return -1;
+  if(super.data_index!=super.root_index+1)
+    return -1;
+  
+  
+  ourFAT.arr = (uint16_t *)malloc(BLOCK_SIZE*super.num_FAT*sizeof(uint16_t));
+  void * temp = (void *) malloc(BLOCK_SIZE);
+  for(i=1; i < super.root_index;i++)
+  {
+    block_read(i,temp);
+    memcpy(ourFAT.arr+4096*(i-1),temp,BLOCK_SIZE);
+  }
+  if(ourFAT.arr[0]!= 0xFFFF)
+    return -1;
+  mount_buffer = (void *)malloc(BLOCK_SIZE);
+  our_root = (struct rootdirectory *)malloc(sizeof(struct rootdirectory));
+  block_read(super.root_index,mount_buffer);
+  
+  for (i=0;i<FS_FILE_MAX_COUNT;i++)
+  {
+    struct entries * entry = (struct entries*)malloc(sizeof(struct entries));
+    memcpy(entry,mount_buffer+(i*32),32); 
+    our_root->root[i] = *entry; 
+  }
+  
+  return 0;
 }
 
 int fs_umount(void)
 {
-	/* TODO: Phase 1 */
+	int i;
+  void * umount_buffer=malloc(sizeof(BLOCK_SIZE));
+  for(i=0;i<super.num_FAT;i++)
+  {
+    memcpy(umount_buffer,ourFAT.arr+i*4096,BLOCK_SIZE);
+    block_write(1+i,umount_buffer);
+  }
+  block_write(super.root_index,our_root);
+  
+  int close = block_disk_close();
+    
+  if (close == -1)
+    return -1;
+  if(our_fd_table!=NULL)
+  {
+    if(our_fd_table->fd_count!=0)
+      return -1;
+  }
+  return 0;
 }
 
 int fs_info(void)
 {
-	/* TODO: Phase 1 */
+	if(block_disk_count()==-1)
+    return -1;
+  int freefat = 0, i;
+  int root = 0;
+  
+  printf("File System Info:\n");
+  printf("Total Block # %i\n",super.total_amount);
+  printf("FAT Block # %i\n",super.num_FAT);
+  printf("Root Directory Index: %i\n",super.root_index);
+  printf("Data Block Index: %i\n",super.data_index);
+  printf("Data Block # %i\n",super.num_data);
+  
+  for(i=0;i < super.num_data;i++)
+  {
+    if(ourFAT.arr[i] == 0 )
+    {
+      freefat++;
+    }
+  }
+  
+  printf("Free FAT Blocks # %d/%d\n",freefat,super.num_data);
+  for(i=0; i < FS_FILE_MAX_COUNT; i++)
+  {
+    struct entries node;
+    node = our_root->root[i];
+    if (strlen(node.filename) == 0)
+      root++;
+  }
+  
+  printf("Free Root Entries # %d/%d\n",root ,FS_FILE_MAX_COUNT);
+  
+  return 0;
 }
-
-
-
-
-
-
-
-
 
 //done?
 /**
@@ -100,9 +177,7 @@ int fs_create(const char *filename)
   int count = 0;
   int valid = 1;
 
-
   /* TODO: Phase 2 */
-
 
   //checks for validity of filename
   for(i = 0; i < FS_FILENAME_LEN; i++){
@@ -148,8 +223,6 @@ int fs_create(const char *filename)
   }
   return 0;
 }
-
-
 
 /**
  * fs_delete - Delete a file
@@ -197,9 +270,6 @@ int fs_delete(const char *filename)
   }
 }
 
-
-
-
 /**
  * fs_ls - List files on file system
  *
@@ -211,22 +281,6 @@ int fs_ls(void)
 {
 	/* TODO: Phase 2 */
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 int fs_open(const char *filename)
 {
